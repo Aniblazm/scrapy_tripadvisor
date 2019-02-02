72e95a13db4565481b7e96bc431a505b880eec89
Anibal Blazquez <ablazquezmarcos@gmail.com>
2019-02-02 12:37:28 +0100

    Tripadvisor scrape with scrapy
    If you need to get info about the best hotels in your city, you can use this software to get:
    - The list of hotels listed in Tripadvisor.
    - The number of reviews.
    - The average rating of each hotel.
    
    In case you need more info from the tripadvisor website, you can use this script as a template and add the new info you desire to get.

---
+++ spiders/tripadvisor_barcelona.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from datetime import datetime
+
+class TripadvisorBarcelonaSpider(scrapy.Spider):
+    name = 'tripadvisor_barcelona'
+    allowed_domains = ['tripadvisor.com']
+    start_urls = ['https://www.tripadvisor.com/Hotels-g187497-Barcelona_Catalonia-Hotels.html']
+
+    def parse(self, response):
+        # Extract data using xpath
+        hotel_name = response.xpath('//div[@class="prw_rup prw_meta_hsx_listing_name listing-title"]/div[@class="listing_title"]/a[@class="property_title prominent "]/text()').extract()
+        review_count = response.xpath('//a[@class="review_count"]/text()').extract()
+        review_avg = response.css('[class~="ui_bubble_rating"]::attr(alt)').extract()
+
+        row_data = zip(hotel_name, review_count, review_avg)
+
+        # Making extracted data row wise
+        for item in row_data:
+            # create a dictionary to store the scraped info
+            scraped_info = {
+                # key:value
+                'page': response.url,
+                'hotel_name': item[0],
+                # item[0] means product in the list and so on, index tells what value to assign
+                'review_count': item[1],
+                'review_avg': item[2]
+            }
+
+            # yield or give the scraped info to scrapy
+            yield scraped_info
+
+            NEXT_PAGE_SELECTOR = '[class="nav next taLnk ui_button primary"]::attr(href)'
+            next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+
+            if next_page:
+                yield scrapy.Request(
+                    response.urljoin(next_page),
+                    callback=self.parse)
